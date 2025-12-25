ee3c2027323e81c9fc57d5fed3e34cc5766f3994
SelinaWang-24998 <2499814010@qq.com>
2025-12-25 15:33:01 +0800

    fix:管理员一登录跳转到/admin/profile界面

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -156,7 +156,7 @@ async function handleLogin() {
 
       switch (res.data.roleType) {
         case 'admin':
-          router.push('/admin/dashboard')
+          router.push('/admin/profile')
           break
         case 'doctor':
           router.push('/doctor/profile')
@@ -165,13 +165,13 @@ async function handleLogin() {
           router.push('/department')
       }
     } else {
-      alert(res.msg || '登录失败')
+      alert(res.message || '登录失败')
       loadCaptcha()
       captchaCode.value = ''
     }
   } catch (err) {
     console.error(err)
-    alert(err?.response?.data?.msg || '服务器连接错误')
+    alert(err?.response?.data?.message || '服务器连接错误')
     loadCaptcha()
     captchaCode.value = ''
   }
